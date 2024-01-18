dd278b77536ab393353ca0bd6ca823c1c1081986
TengHon <e0543465@u.nus.edu>
2024-01-18 16:33:52 +0800

    Add base seed data

--- sipsuggest_backend/db/seeds.rb
+++ sipsuggest_backend/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Create Ingredients
+mint = Ingredient.find_or_create_by(name: "Mint")
+lime = Ingredient.find_or_create_by(name: "Lime")
+rum = Ingredient.find_or_create_by(name: "Rum")
+soda = Ingredient.find_or_create_by(name: "Soda")
+
+# Create Cocktail Recipes
+mojito = Recipe.create(name: "Mojito")
+rum_punch = Recipe.create(name: "Rum Punch")
+
+
+# Associate Ingredients with Cocktail Recipes
+mojito.ingredients << [mint, lime, rum]
+rum_punch.ingredients << [rum, lime, soda]
+
+puts "Seed data has been created successfully."
