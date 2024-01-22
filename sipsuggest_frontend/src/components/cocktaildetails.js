b1f04da780a8ec2d71f3a767af4eb72ea123d714
TengHon <e0543465@u.nus.edu>
2024-01-22 15:38:44 +0800

    Add checkbox for instructions

--- sipsuggest_frontend/src/components/cocktaildetails.js
+++ sipsuggest_frontend/src/components/cocktaildetails.js
@@ -1,33 +1,57 @@
-import React from 'react'
-import { Box, Button, ListItem, ListItemText, Typography, List, ListItemIcon } from '@mui/material';
+import React, { useState } from 'react'
+import { Box, Button, ListItem, ListItemText, Typography, List, ListItemIcon, Checkbox } from '@mui/material';
 import StarBorder from '@mui/icons-material/StarBorder';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 
 
 
 function CocktailDetails(data) {
-
-  const ingredients = data.data.ingredients;
-
+ 
   const onClick = (value) => {
     console.log(data.data.steps);
       
   }  
 
+  // const [checked, setChecked] = useState([]);
+
+  // const handleToggle = (value) => {
+  //   const currentIndex = checked.indexOf(value);
+  //   const newChecked = [...checked];
+
+  //   if (currentIndex === -1) {
+  //     newChecked.push(value);
+  //   } else {
+  //     newChecked.splice(currentIndex, 1);
+  //   }
+
+  //   setChecked(newChecked);
+  // };
+
   const listIngredients = data.data.ingredients.map( ingredient => {
     return (
-        <ListItem key={ingredient.id}>
+        <ListItem 
+          key={ingredient.id}
+        >
           <ListItemIcon>
             <ArrowRightIcon/>
           </ListItemIcon>
-            <ListItemText primary={ingredient.name}/>
+            <ListItemText 
+              primary={ingredient.name}
+            />
         </ListItem>
     )
   });
 
   const listSteps = data.data.steps.map( step => {
     return (
-        <ListItem key={step.id}>
+        <ListItem 
+          key={step.id}
+          secondaryAction={
+            <Checkbox
+              edge='end'
+            />
+          }
+        >
           <ListItemIcon>
             <ArrowRightIcon/>
           </ListItemIcon>
