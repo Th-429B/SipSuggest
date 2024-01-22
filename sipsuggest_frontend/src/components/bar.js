32c2058e80c0d33c4fa698301d93d7d7332b5fec
TengHon <e0543465@u.nus.edu>
2024-01-22 15:28:09 +0800

    Add helper text for seach bar

--- sipsuggest_frontend/src/components/bar.js
+++ sipsuggest_frontend/src/components/bar.js
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
 import InputAdornment from '@mui/material/InputAdornment';
+import { Typography } from '@mui/material';
 
 
 function Bar( {onSearchTextChange} ) {
@@ -10,14 +11,15 @@ function Bar( {onSearchTextChange} ) {
   return (
     <Box       
       sx={{
-        width: '100%',
+        // width: '100%',
       }}
     >
-      <TextField id="outlined-basic" variant="outlined" fullWidth       
+      <TextField id="outlined-basic" variant="outlined"        
         sx={{
           paddingRight: '10px',
           paddingLeft: '10px',
-          boxSizing: 'border-box' // box sizing tells browser to include padding to total width so we don't have additional horizontal scroll
+          boxSizing: 'border-box', // box sizing tells browser to include padding to total width so we don't have additional horizontal scroll
+          width: '60%'
         }}
         onChange={(e) => onSearchTextChange(e.target.value)}  
         InputProps={{
@@ -27,6 +29,7 @@ function Bar( {onSearchTextChange} ) {
             </InputAdornment>
           ),
         }}
+        helperText='Enter an ingredient name'
       />
     </Box>
   );
