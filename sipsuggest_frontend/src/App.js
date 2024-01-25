76dcd4f7b51971b733514144bf2298527cc48415
TengHon <e0543465@u.nus.edu>
2024-01-25 15:25:46 +0800

    Add react router

--- sipsuggest_frontend/src/App.js
+++ sipsuggest_frontend/src/App.js
@@ -3,27 +3,8 @@ import axios from 'axios';
 import CocktailTable from './components/cocktailtable';
 import { useEffect, useState } from 'react';
 
-const API_URL = 'http://localhost:3000/recipes'
-
-// function getAPIData() {
-//   return axios.get(API_URL).then((resp) => resp.data)
-// }
-
 function App() {
-  // const [recipes, setRecipes] = useState([])
-  
-  // useEffect(()=> {
-  //   let mounted = true;
-  //   getAPIData().then((items) => {
-  //     if (mounted) {
-  //       setRecipes(items);
-  //       console.log(items)
-  //     }
-  //   });
-  //   return () => (mounted = false);
 
-  // }, []);
-  
   return (
     <div className="App">
       <h1>SipSuggest</h1>    
